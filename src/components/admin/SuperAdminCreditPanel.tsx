871878545d7da5a53275e2c2d4376be41519c4e8
alexbuenhombre2012 <alexbuenhombre2012@gmail.com>
2025-06-14 12:40:20 -0500

    Update SuperAdminCreditPanel.tsx

--- src/components/admin/SuperAdminCreditPanel.tsx
+++ src/components/admin/SuperAdminCreditPanel.tsx
@@ -1,4 +1,5 @@
 
+import { useAuth } from '@/contexts/AuthContext';
 import React, { useState, useEffect } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
