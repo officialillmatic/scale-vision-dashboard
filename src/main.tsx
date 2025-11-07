364d58ff83a80dd914a370f7f3ce347011ec04e3
alexbuenhombre2012 <alexbuenhombre2012@gmail.com>
2025-11-06 23:52:35 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { GlobalErrorBoundary } from '@/components/common/GlobalErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalErrorBoundary>
-      <App />
-    </GlobalErrorBoundary>
+    <App />
   </React.StrictMode>,
 )
